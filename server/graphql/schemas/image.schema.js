cbcf8106a16a3ff4453aff0953c22aff3dc278c0
javadoge <ramechung@gmail.com>
2022-01-25 13:31:53 +0800

    it's good

--- server/graphql/schemas/image.schema.js
+++
@@ -1,6 +0,0 @@
-module.exports = `extend type Query {
-   get_info: String!
- }
- extend type Mutation {
-   image_uploader(file: Upload!): String
- }`;
--- server/graphql/subscription.js
+++
@@ -1,3 +0,0 @@
-type Subscription {
-    bidPlaced: Item
-  }
